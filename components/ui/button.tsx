f782b9e9a2a5115e152cdb4667657b734739d637
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-07-13 19:44:28 +0700

    chore: Add new button styles for green and white

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        green: "btn_green rounded-full hover:bg-green-50",
+        white: "btn_white_text rounded-full hover:bg-white",
       },
       size: {
         default: "h-10 px-4 py-2",
