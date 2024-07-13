307e79bda03ea12d07ad2994ca99e1728d9119bc
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-07-13 23:01:24 +0700

    refactor: Add @vercel/analytics for page tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
 import Navbar from '@/components/Navbar';
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
