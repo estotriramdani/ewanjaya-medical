c0a788f1ae1552187fa4b9f79a39687abb20020e
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-07-13 19:00:03 +0700

    integration with shadcn and create mobile nav

---
+++ components/MobileNav.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { NAV_LINKS } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useRef, useState } from 'react';
+
+const MobileNav = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+  const navListRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (toggleNav) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [toggleNav]);
+
+  return (
+    <>
+      <button onClick={() => setToggleNav((prev) => !prev)}>
+        <Image
+          src="menu.svg"
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
+        />
+      </button>
+
+      {toggleNav && (
+        <div className="lg:hidden fixed inset-0 shadow-lg flex flex-col">
+          <div className="bg-white py-8">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="flexCenter regular-16 py-4 hover:text-gray-500"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+          <div className="flex-1 bg-gray-50/50" onClick={() => setToggleNav(false)}></div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MobileNav;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         green: {
@@ -28,6 +39,39 @@ module.exports = {
         yellow: {
           50: '#FEC601',
         },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       backgroundImage: {
         'bg-img-1': "url('/img-1.png')",
@@ -41,13 +85,32 @@ module.exports = {
         '3xl': '1680px',
         '4xl': '2200px',
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+        '5xl': '40px',
+      },
       maxWidth: {
         '10xl': '1512px',
       },
-      borderRadius: {
-        '5xl': '40px',
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
